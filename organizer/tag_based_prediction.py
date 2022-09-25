47696635a7d15fb3dc8967cc03f3684691709f3e
ss0201 <ss0201.777@gmail.com>
2022-09-25 21:36:50 +0900

    feat(tag_pred): dataframe import, tree params

--- organizer/tag_based_prediction.py
+++ organizer/tag_based_prediction.py
@@ -15,18 +15,28 @@ def main():
         description='Predict classification based on tags.')
     parser.add_argument('project_dir')
     parser.add_argument('input_dirs', nargs='+')
-    parser.add_argument('--output', default='.',  dest='output_dir')
+    parser.add_argument('--output', default='.', dest='output_dir')
+    parser.add_argument('--dataframe', required=False)
+    parser.add_argument('--max-depth', type=int, required=False)
+    parser.add_argument('--min-leaf', type=int, required=False)
+
     args = parser.parse_args()
 
-    print('Creating dataframe...')
-    tags = list(set(EXPLICIT_TAGS + QUESTIONABLE_TAGS + SAFE_TAGS))
-    df = create_dataframe(args.project_dir, args.input_dirs, tags)
-    print(df)
-    output_dataframe(df, args.output_dir)
+    if args.dataframe:
+        df: pd.DataFrame = pd.read_pickle(args.dataframe)
+    else:
+        print('Creating dataframe...')
+        tags = list(set(EXPLICIT_TAGS + QUESTIONABLE_TAGS + SAFE_TAGS))
+        df = create_dataframe(args.project_dir, args.input_dirs, tags)
+        print(df)
+        output_dataframe(df, args.output_dir)
 
     print('Creating decision tree...')
-    classifier = create_decision_tree(df)
+    classifier = create_decision_tree(
+        df, args.max_depth, args.min_leaf)
     output_decision_tree(classifier, df, args.output_dir)
+
+    print('Done.')
 
 
 def create_dataframe(project_dir: str, image_dirs: list[str], tags: list[str]) -> pd.DataFrame:
@@ -51,8 +61,9 @@ def output_dataframe(df: pd.DataFrame, output_dir: str) -> None:
     df.to_pickle(os.path.join(output_dir, 'dataframe.pkl'))
 
 
-def create_decision_tree(df: pd.DataFrame) -> tree.DecisionTreeClassifier:
-    classifier = tree.DecisionTreeClassifier()
+def create_decision_tree(df: pd.DataFrame, max_depth: int, min_samples_leaf: int) -> tree.DecisionTreeClassifier:
+    classifier = tree.DecisionTreeClassifier(
+        max_depth=max_depth, min_samples_leaf=min_samples_leaf)
     return classifier.fit(df.drop([CLASS_COLUMN], axis=1).to_numpy(), df[CLASS_COLUMN])
 
 
